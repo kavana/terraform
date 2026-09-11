020263ae1bc40437288922924160a94ca3885cfa
Kavana SK <kavana.sk@gmail.com>
2026-09-11 09:19:56 -0700

    Update main.tf

--- vsphere/main.tf
+++ vsphere/main.tf
@@ -1,54 +1,41 @@
 provider "vsphere" {
-  # If you have a self-signed cert
+  user                 = "tango.automation@vsphere.local"
+  password             = "3kk7*08xEtmu"
   allow_unverified_ssl = true
+  api_timeout          = 10
 }
 
-data "vsphere_datacenter" "dc" {
-  name = "ESO_PKS_VC01_DC01"
+data "vsphere_datacenter" "datacenter" {
+  name = "vcfcons-mgmt-vc03-DC"
 }
 
 data "vsphere_datastore" "datastore" {
-  name          = "vsanDatastore_cluster01"
-  datacenter_id = data.vsphere_datacenter.dc.id
+  name          = "datastore1 (5)"
+  datacenter_id = data.vsphere_datacenter.datacenter.id
 }
 
-data "vsphere_resource_pool" "pool" {
-  name          = "tango-e2e-vm-pool"
-  datacenter_id = data.vsphere_datacenter.dc.id
+data "vsphere_compute_cluster" "cluster" {
+  name          = "vcfcons-vc03-cl01"
+  datacenter_id = data.vsphere_datacenter.datacenter.id
 }
 
 data "vsphere_network" "network" {
-  name          = "ESO_PKS_VC01_VM"
-  datacenter_id = data.vsphere_datacenter.dc.id
+  name          = "VM Network"
+  datacenter_id = data.vsphere_datacenter.datacenter.id
 }
 
-data "vsphere_virtual_machine" "template" {
-  name          = "TestCentos"
-  datacenter_id = "${data.vsphere_datacenter.dc.id}"
-}
-
-resource "vsphere_virtual_machine" "cloned_virtual_machine" {
-  name             = "terraform-test-gadagip"
-  resource_pool_id = data.vsphere_resource_pool.pool.id
+resource "vsphere_virtual_machine" "vm" {
+  name             = "terraform-test"
+  resource_pool_id = data.vsphere_compute_cluster.cluster.resource_pool_id
   datastore_id     = data.vsphere_datastore.datastore.id
-
-  num_cpus = 1
-  memory   = 1024
-  guest_id = data.vsphere_virtual_machine.template.guest_id
-
-  scsi_type = data.vsphere_virtual_machine.template.scsi_type
-
+  num_cpus         = 1
+  memory           = 1024
+  guest_id         = "otherLinux64Guest"
   network_interface {
-    network_id   = data.vsphere_network.network.id
-    adapter_type = data.vsphere_virtual_machine.template.network_interface_types[0]
+    network_id = data.vsphere_network.network.id
   }
-
   disk {
     label = "disk0"
-    size = 10
-  }
-
-  clone {
-    template_uuid = data.vsphere_virtual_machine.template.id
+    size  = 20
   }
 }
